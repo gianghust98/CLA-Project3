9018e82a7bd373a5ba5a353b7bd16646f2e32f5c
Hương Giang <huonggiang@MacBook-Pro-cua-Huong.local>
2020-11-21 13:00:44 +0700

    add feature middleware

--- backend/app/models/index.js
+++ backend/app/models/index.js
@@ -6,6 +6,7 @@ mongoose.Promise = global.Promise
 const db = {}
 db.mongoose = mongoose
 db.url = dbConfig.url
+
 db.banners = require('./banner.model')(mongoose)
 db.courses = require('./course.model')(mongoose)
 db.course_groups = require('./courseGroup.model')(mongoose)
@@ -14,5 +15,10 @@ db.news = require('./new.model')(mongoose)
 db.students_need_advise = require('./studentNeedAdvise.model')(mongoose)
 db.teachers = require('./teacher.model')(mongoose)
 db.tests = require('./test.model')(mongoose)
+
+db.user = require('./user.model')
+db.role = require('./role.model')
+
+db.ROLES = ["user", "admin", "moderator"]
 
 module.exports = db
