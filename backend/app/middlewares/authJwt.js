b6d6df6ff4f9ed6bdfb68423d353f3e4b422cdae
hglinh98 <hlinhlinh140898@gmail.com>
2020-12-12 00:01:40 +0700

    giao dien admin va banner cua admin

--- backend/app/middlewares/authJwt.js
+++ backend/app/middlewares/authJwt.js
@@ -1,84 +1,84 @@
-const jwt = require('jsonwebtoken')
-const config = require('../config/auth.config')
-const db = require('../models')
-const User = db.user
-const Role = db.role
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
 
 verifyToken = (req, res, next) => {
-  let token  = req.headers["x-access-token"]
+  let token = req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(403).send({ message: "No token provided!" })
+    return res.status(403).send({ message: "No token provided!" });
   }
   jwt.verify(token, config.secret, (err, decoded) => {
     if (err) {
-      return res.status(401).send({ message: "Unauthorized!" })
+      return res.status(401).send({ message: "Unauthorized!" });
     }
-    req.userId = decoded.id 
-    next()
-  })
-}
+    req.userId = decoded.id;
+    next();
+  });
+};
 
 isAdmin = (req, res, next) => {
   User.findById(req.userId).exec((err, user) => {
     if (err) {
-      res.status(500).send({ message: err })
-      return
+      res.status(500).send({ message: err });
+      return;
     }
     Role.find(
       {
-        _id: { $in: user.roles }
+        _id: { $in: user.roles },
       },
       (err, roles) => {
         if (err) {
-          res.status(500).send({ message: err })
-          return
+          res.status(500).send({ message: err });
+          return;
         }
         for (let i = 0; i < roles.length; i++) {
-          if (roles[i].name === "admin"){
-            next()
-            return
+          if (roles[i].name === "admin") {
+            next();
+            return;
           }
         }
-        res.status(403).send({ message: "Require Admin Role!" })
-        return
+        res.status(403).send({ message: "Require Admin Role!" });
+        return;
       }
-    )
-  })
-}
+    );
+  });
+};
 
 isModerator = (req, res, next) => {
   User.findById(req.userId).exec((err, user) => {
     if (err) {
-      res.status(500).send({ message: err })
-      return
+      res.status(500).send({ message: err });
+      return;
     }
     Role.find(
       {
-        _id: { $in: user.roles }
+        _id: { $in: user.roles },
       },
       (err, roles) => {
         if (err) {
-          res.status(500).send({ message: err })
-          return
+          res.status(500).send({ message: err });
+          return;
         }
         for (let i = 0; i < roles.length; i++) {
           if (roles[i].name === "moderator") {
-            next()
-            return
+            next();
+            return;
           }
         }
-        res.status(403).send({ message: "Require Moderator Role!" })
-        return
+        res.status(403).send({ message: "Require Moderator Role!" });
+        return;
       }
-    )
-  })
-}
+    );
+  });
+};
 
 const authJwt = {
   verifyToken,
   isAdmin,
-  isModerator
-}
+  isModerator,
+};
 
-module.exports = authJwt
+module.exports = authJwt;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,9 +11,13 @@ import News from './components/News';
 import Contact from './components/Contact';
 // import Login from './components/Login';
 // import Signup from './components/Signup';
-import Login from './components/Form/Login'
-import Signup from './components/Form/Register'
+// import Login from './components/Form/Login'
+// import Signup from './components/Form/Register'
 import Profile from './components/Form/Profile'
+// import Login from './components/Login';
+// import Signup from './components/Signup';
+import Admin from './components/Admin/Admin';
+import BannerAdmin from './components/Admin/BannerAdmin';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 
@@ -31,7 +35,12 @@ function App() {
       {/* <Route exact path="/login" component={Login} /> */}
       {/* <Route exact path="/signup" component={Signup} /> */}
       <Route exact path="/profile" component={Profile} />
+      {/* <Route exact path="/login" component={Login} /> */}
+      {/* <Route exact path="/signup" component={Signup} /> */}
+      <Route exact path="/admin" component={Admin} />
+      <Route exact path="/admin/banners" component={BannerAdmin} />
       <Footer />
+     
     </Router>
   );
 }
